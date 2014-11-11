41f0aed8300e46542b49e403691cbdae62904088
Dawson Toth <dtoth@appcelerator.com>
2014-11-11 14:56:44 -0500

    Full Storage CRUD
    Yay for inaccurate documentation...

--- test/connector.js
+++ test/connector.js
@@ -1,4 +1,5 @@
 var should = require('should'),
+	assert = require('assert'),
 	APIBuilder = require('apibuilder'),
 	config = new APIBuilder.Loader(),
 	Connector = require('../lib');
@@ -9,6 +10,7 @@ describe('Connector', function() {
 		Model;
 
 	before(function(next) {
+		var self = this;
 		should.notEqual(config.azure_account, 'YOUR_AZURE_ACCOUNT', 'Please configure an account and key!');
 		should.notEqual(config.azure_key, 'YOUR_AZURE_KEY', 'Please configure an account and key!');
 		connector = new Connector();
@@ -30,10 +32,16 @@ describe('Connector', function() {
 		});
 		should(Model).be.ok;
 
-		connector.connect(function(err) {
+		connector.connect(function connectCallback(err) {
 			should(err).be.not.ok;
 
 			Model.createTableIfNotExists(function(err, result) {
+				if (err && String(err).indexOf('The specified table is being deleted. Try operation later') >= 0) {
+					self.timeout(2 * 60 * 1000);
+					console.log('delaying create table until last test run finishes cleaning up...');
+					return setTimeout(connectCallback, 10000);
+				}
+
 				should(err).be.not.ok;
 				next();
 			});
@@ -59,16 +67,30 @@ describe('Connector', function() {
 		});
 	});
 
+	it('should be able to find all', function(next) {
+		var object = createObject();
+		Model.create(object, function(err, instance) {
+			should(err).be.not.ok;
+			should(instance).be.an.Object;
+			Model.findAll(function(err, coll) {
+				should(err).be.not.ok;
+				shouldContain(coll, instance);
+				instance.delete(next);
+			});
+		});
+	});
+
 	it('should be able to find an instance by ID', function(next) {
 		var object = createObject();
 		Model.create(object, function(err, instance) {
 			should(err).be.not.ok;
 			should(instance).be.an.Object;
 			var id = instance.getPrimaryKey();
-			Model.find(id, function(err, instance2) {
+			Model.findOne(id, function(err, instance2) {
 				should(err).be.not.ok;
 				should(instance2).be.an.Object;
-				should(instance2.getPrimaryKey()).deepEqual(id);
+				assert.deepEqual(instance.getPrimaryKey(), instance2.getPrimaryKey());
+				should(instance2.getPrimaryKey()).be.an.Object;
 				should(instance2.Make).equal(object.Make);
 				instance.delete(next);
 			});
@@ -89,7 +111,7 @@ describe('Connector', function() {
 		});
 	});
 
-	it('should be able to sort, limit, skip while finding', function(next) {
+	it('should be able to sel, limit while finding', function(next) {
 		var limit = 3,
 			object = createObject();
 		Model.create(object, function(err, instance) {
@@ -98,9 +120,7 @@ describe('Connector', function() {
 			var options = {
 				where: { Make: object.Make },
 				sel: { Id: 1, Make: 1 },
-				order: { Id: 1, Make: 1 },
-				limit: limit,
-				skip: 0
+				limit: limit
 			};
 			Model.query(options, function(err, coll) {
 				should(err).be.not.ok;
@@ -124,7 +144,7 @@ describe('Connector', function() {
 				instance2.save(function(err, result) {
 					should(err).be.not.ok;
 					should(result).be.an.Object;
-					should(result.getPrimaryKey()).deepEqual(id);
+					assert.deepEqual(instance.getPrimaryKey(), instance2.getPrimaryKey());
 					should(result.Make).equal(newMake);
 					instance.delete(next);
 				});
