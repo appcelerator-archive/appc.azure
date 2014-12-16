11342af5334276172c3c89937baeb5eff5943123
Dawson Toth <dtoth@appcelerator.com>
2014-12-16 13:48:31 -0500

    https://jira.appcelerator.org/browse/API-230

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
+/**
+ * NOTE: This file is simply for testing this connector and will not
+ * be used or packaged with the actual connector when published.
+ */
 var APIBuilder = require('appcelerator').apibuilder,
-	server = new APIBuilder(),
-	connector = server.getConnector('appc.azure');
-
-// lifecycle examples
-server.on('starting', function() {
-	server.logger.info('server is starting!');
-});
-
-server.on('started', function() {
-	server.logger.info('server started!');
-});
-
-//--------------------- implement authorization ---------------------//
-
-// fetch our configured apikey
-var apikey = server.get('apikey');
-server.logger.info('APIKey is:', apikey);
-
-function APIKeyAuthorization(req, resp, next) {
-	if (!apikey) {
-		return next();
-	}
-	if (req.headers['apikey']) {
-		var key = req.headers['apikey'];
-		if (key == apikey) {
-			return next();
-		}
-	}
-	resp.status(401);
-	return resp.json({
-		id: "com.appcelerator.api.unauthorized",
-		message: "Unauthorized",
-		url: ""
-	});
-}
-
-//--------------------- simple user model ---------------------//
+	server = new APIBuilder();
 
 var Car = APIBuilder.Model.extend('Car', {
 	fields: {
@@ -53,14 +21,8 @@ var Car = APIBuilder.Model.extend('Car', {
 	},
 	connector: 'appc.azure'
 });
-
-// add an authorization policy for all requests at the server log
-server.authorization = APIKeyAuthorization;
-
-// create a user api from a user model
 server.addModel(Car);
 
-// start the server
 server.start(function(err) {
 	if (err) {
 		return server.logger.fatal(err);
