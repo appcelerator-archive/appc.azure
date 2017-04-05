b871116c4194a65f57ba0ab258c3b3fb70221592
vladimir-trifonov <trifonov.dev@gmail.com>
2017-04-05 15:26:48 +0300

    Gruntbuild changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,30 +4,15 @@ module.exports = function (grunt) {
     mochaTest: {
       options: {
         timeout: 30000,
-        reporter: 'spec',
-        ignoreLeaks: true
+        reporter: 'spec'
       },
-      src: ['test/**/*.js']
-    },
-    jshint: {
-      options: {
-        jshintrc: true
-      },
-      src: ['lib/**/*.js', 'test/**/*.js']
-    },
-    kahvesi: {
-      src: ['test/**/*.js']
-    },
-    clean: ['tmp']
+      src: ['test/integration/**/*.js']
+    }
   })
 
   // Load grunt plugins for modules
   grunt.loadNpmTasks('grunt-mocha-test')
-  grunt.loadNpmTasks('grunt-contrib-jshint')
-  grunt.loadNpmTasks('grunt-contrib-clean')
-  grunt.loadNpmTasks('grunt-kahvesi-latest')
 
   // register tasks
-  grunt.registerTask('cover', ['kahvesi', 'clean'])
-  grunt.registerTask('default', ['jshint', 'mochaTest', 'clean'])
+  grunt.registerTask('default', ['mochaTest'])
 }
